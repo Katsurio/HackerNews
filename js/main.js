ae180a578655bc3efa8c816737b55aad868bc731
John (Jef) Fisher <Fishe545@gmail.com>
2021-08-07 18:11:08 +0800

    feat: added form for adding stories and click handler is working

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ const $navLogin = $('#nav-login')
 const $navUserProfile = $('#nav-user-profile')
 const $navLogOut = $('#nav-logout')
 
+const navSubmitStory = document.getElementById('nav-submit-story')
+const $navSubmitStory = $('#nav-submit-story')
+const addStoryForm = document.getElementById('add-story-form')
+
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
@@ -22,6 +26,7 @@ const $navLogOut = $('#nav-logout')
 function hidePageComponents() {
   const components = [$allStoriesList, $loginForm, $signupForm]
   components.forEach((c) => c.hide())
+  // navSubmitStory.classList.toggle('hidden')
 }
 
 /** Overall function to kick off the app. */
--- js/nav.js
+++ js/nav.js
@@ -32,5 +32,20 @@ function updateNavOnLogin() {
   $('.main-nav-links').show()
   $navLogin.hide()
   $navLogOut.show()
+  $loginForm.hide()
+  $signupForm.hide()
   $navUserProfile.text(`${currentUser.username}`).show()
+  const navSubmitStory = document.getElementById('nav-submit-story')
+  const addStoryForm = document.getElementById('add-story-form')
+  navSubmitStory.classList.toggle('hidden')
+}
+navSubmitStory.addEventListener('click', navSubmitClick)
+/** Show add story form on click "submit". */
+
+function navSubmitClick(evt) {
+  console.log('CLICKED', evt)
+  console.debug('navSubmitClick')
+  hidePageComponents()
+  const addStoryForm = document.getElementById('add-story-form')
+  addStoryForm.classList.toggle('hidden')
 }
